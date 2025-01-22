c39d6078ebad48b52149173abeb6ddaa15765035
sid <sid98816@gmail.com>
2025-01-22 22:04:03 +0900

    design: 반응형 css 적용

--- app/page.tsx
+++ app/page.tsx
@@ -39,14 +39,14 @@ export default async function Home() {
     }
   }
   return (
-    <div className="bg-slate-700 grid items-center min-h-screen gap-16 sm:p-2 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-slate-900 overflow-x-auto grid items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-center gap-4 row-start-2 items-start">
-        <section className="mb-14 w-full max-w-[1016px] text-psbk desktop:w-full desktop:min-w-[1016px]">
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-900">
-              <caption className="p-5 text-xl font-semibold text-left rtl:text-right text-gray-900 bg-white">
+        <section className="mb-6 md:mb-14 w-full max-w-[1016px] desktop:w-full">
+          <div className="relative overflow-x-auto sm:rounded-lg">
+            <table className="w-full text-xs md:text-sm text-left rtl:text-right text-gray-900">
+              <caption className="p-3 md:p-5 text-lg md:text-xl font-semibold text-left rtl:text-right text-gray-900 bg-white">
                 기술 지표 기반 코인 종목 추천
-                <p className="mt-1 text-sm font-normal text-gray-600">
+                <p className="mt-1 text-xs md:text-sm font-normal text-gray-600 ">
                   <a href="https://upbit.com/home" className="underline">
                     업비트
                   </a>{" "}
@@ -58,65 +58,67 @@ export default async function Home() {
                   <span className="text-blue-600"> 푸를수록 </span>
                   매도를 추천하는 종목입니다.
                 </p>
-                <p className="mt-1 text-sm font-normal text-gray-600">
+                <p className="mt-1 text-xs font-normal text-gray-600 md:text-sm">
                   하루 전날({data ? data[0].candle_date : "데이터 없음"})까지의
                   데이터를 기반으로 지표를 계산하며, 한국 시간 기준 매일 오전
                   0시 이후에 데이터를 갱신합니다.
                 </p>
-                <p className="mt-1 text-sm font-normal text-gray-400">
+                <p className="mt-1 text-xs md:text-sm font-normal text-gray-400">
                   이동평균선(MA): 일정 기간 동안의 평균 가격을 의미하며 가격
                   변동과 추세를 파악하기 위한 지표입니다.
                 </p>
-                <p className="mt-1 text-sm font-normal text-gray-400">
+                <p className="mt-1 text-xs md:text-sm font-normal text-gray-400">
                   RSI: 일정 기간 동안의 상승폭과 하락폭을 비교하는 지표이며
                   일반적으로 70 이상은 과매수, 30 이하는 과매도로 간주합니다.
                 </p>
               </caption>
               <thead className="text-xs text-gray-800 uppercase bg-gray-50">
                 <tr>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-2 md:px-6 py-2 md:py-3">
                     종목 (마켓)
                   </th>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-2 md:px-6 py-2 md:py-3">
                     전날 종가
                   </th>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-2 md:px-6 py-2 md:py-3">
                     전날 변동가
                   </th>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-6 py-3 hidden md:table-cell">
                     MA_7
                   </th>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-6 py-3 hidden md:table-cell">
                     MA_14
                   </th>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-6 py-3 hidden md:table-cell">
                     MA_50
                   </th>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-6 py-3 hidden md:table-cell">
                     RSI_14
                   </th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="">
                 {data.map((coin, index) => (
                   <tr
                     key={coin.name_code}
-                    className={`${getRowColor(coin.score)} border-b`}
+                    className={`${getRowColor(
+                      coin.score
+                    )} border-b transition-colors duration-200 ease-in-out hover:bg-slate-700 hover:text-white`}
                   >
                     <th
                       scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                      className="px-2 md:px-6 py-1 md:py-3 font-medium whitespace-nowrap"
                     >
                       <span>
                         {coin.korean_name} ({coin.name_code}/KRW)
                       </span>
                     </th>
-                    <td className="px-6 py-4">
+                    <td className="px-2 md:px-6 py-1.5 md:py-3">
                       {coin.trade_price > 100
                         ? Number(coin.trade_price).toLocaleString("ko-KR")
                         : coin.trade_price}
                     </td>
-                    <td className="px-6 py-4">
+                    <td className="px-2 md:px-6 py-1.5 md:py-3">
                       {coin.change_price
                         ? Math.abs(coin.change_price) > 100
                           ? roundTo(coin.change_price, 2).toLocaleString(
@@ -125,28 +127,30 @@ export default async function Home() {
                           : coin.change_price
                         : 0}
                     </td>
-                    <td className="px-6 py-4">
+                    <td className="px-6 py-4 hidden md:table-cell">
                       {coin.ma_7
                         ? coin.ma_7 > 100
                           ? roundTo(coin.ma_7, 0).toLocaleString("ko-KR")
                           : coin.ma_7
                         : "-"}
                     </td>
-                    <td className="px-6 py-4">
+                    <td className="px-6 py-4 hidden md:table-cell">
                       {coin.ma_14
                         ? coin.ma_14 > 100
                           ? roundTo(coin.ma_14, 0).toLocaleString("ko-KR")
                           : coin.ma_14
                         : "-"}
                     </td>
-                    <td className="px-6 py-4">
+                    <td className="px-6 py-4 hidden md:table-cell">
                       {coin.ma_50
                         ? coin.ma_50 > 100
                           ? roundTo(coin.ma_50, 0).toLocaleString("ko-KR")
                           : coin.ma_50
                         : "-"}
                     </td>
-                    <td className="px-6 py-4">{coin.rsi ? coin.rsi : "-"}</td>
+                    <td className="px-6 py-4 hidden md:table-cell">
+                      {coin.rsi ? coin.rsi : "-"}
+                    </td>
                   </tr>
                 ))}
               </tbody>
